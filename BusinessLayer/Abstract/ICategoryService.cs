374e441b02c05bc47d6cb574cdb42fd7ffda7c10
bilikhasan <fatherlyy@hotmail.com.tr>
2024-12-01 16:41:13 +0300

    30.bitti.

---
+++ BusinessLayer/Abstract/ICategoryService.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface ICategoryService
+    {
+        List<Category> GetList();
+
+
+    }
+}
