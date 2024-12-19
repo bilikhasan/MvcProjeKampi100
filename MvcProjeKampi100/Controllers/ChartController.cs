b3c10d49487931e9fcad276d343c4e200475cb5f
bilikhasan <fatherlyy@hotmail.com.tr>
2024-12-19 20:01:24 +0300

    94.bitti.

--- MvcProjeKampi100/Controllers/ChartController.cs
+++ MvcProjeKampi100/Controllers/ChartController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EntityLayer.Concrete;
+using MvcProjeKampi100.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +14,43 @@ namespace MvcProjeKampi100.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+
+        public ActionResult CategoryChart()
+        {
+            return Json(BlogList(), JsonRequestBehavior.AllowGet);
+        }
+
+
+
+
+
+
+        public List<CategoryClass> BlogList()
+        {
+            List<CategoryClass> ct = new List<CategoryClass>();
+            ct.Add(new CategoryClass()
+            {
+                CategoryName = "Yazılım",
+                CategoryCount = 8
+            });
+            ct.Add(new CategoryClass()
+            {
+                CategoryName = "Seyahat",
+                CategoryCount = 4
+            });
+            ct.Add(new CategoryClass()
+            {
+                CategoryName = "Teknoloji",
+                CategoryCount = 7
+            });
+            ct.Add(new CategoryClass()
+            {
+                CategoryName = "Spor",
+                CategoryCount = 1
+            });
+            return ct;
         }
     }
 }
